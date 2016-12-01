719bcc08bd334f43db7d39b62a987e1f7ec27380
harrison314 <harrison314@azet.sk>
2016-12-01 17:30:57 +0100

    Add tests for NullInterceptor.

---
+++ src/MassiveDynamicProxyGenerator.Tests/NullInterceptorTests.cs
@@ -0,0 +1,28 @@
+﻿using MassiveDynamicProxyGenerator.Tests.TestInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveDynamicProxyGenerator.Tests
+{
+    [TestClass]
+    public class NullInterceptorTests
+    {
+        [TestMethod]
+        public void NUllAsyncInterceptor_NoTask_ReturnDefault()
+        {
+            ProxygGenerator generator = new ProxygGenerator();
+            IReturnTypes instance = generator.GenerateProxy<IReturnTypes>(new NullInterceptor());
+
+            instance.ShouldNotBeNull();
+
+            instance.CreateSb(string.Empty).ShouldBe(default(StringBuilder));
+            instance.GetLenght(string.Empty).ShouldBe(default(int));
+            instance.GetVoid();
+        }
+    }
+}
