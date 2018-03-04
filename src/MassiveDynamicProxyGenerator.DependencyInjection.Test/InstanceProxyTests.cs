3e43f0160d8b07e99cd4303e26ef7eac19626dcf
harrison314 <harrison314@azet.sk>
2018-03-04 20:20:21 +0100

    Add test for instances proxys.

---
+++ src/MassiveDynamicProxyGenerator.DependencyInjection.Test/InstanceProxyTests.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MassiveDynamicProxyGenerator.DependencyInjection.Test.Services;
+using Moq;
+using Shouldly;
+using MassiveDynamicProxyGenerator.Microsoft.DependencyInjection;
+
+namespace MassiveDynamicProxyGenerator.DependencyInjection.Test
+{
+    [TestClass]
+    public class InstanceProxyTests
+    {
+        [TestMethod]
+        public void AddInstanceProxy_TypeInstance_Create()
+        {
+            Mock<IInstanceProvicer> instanceProviderMock = new Mock<IInstanceProvicer>(MockBehavior.Strict);
+            instanceProviderMock.Setup(t => t.GetInstance()).Returns(new MessageService()).Verifiable();
+
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy(typeof(IMessageService), instanceProviderMock.Object);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+
+            instanceProviderMock.Verify();
+        }
+
+        [TestMethod]
+        public void AddInstanceProxy_TypeInstanceWithLifestyle_Create()
+        {
+            Mock<IInstanceProvicer> instanceProviderMock = new Mock<IInstanceProvicer>(MockBehavior.Strict);
+            instanceProviderMock.Setup(t => t.GetInstance()).Returns(new MessageService()).Verifiable();
+
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy(typeof(IMessageService), instanceProviderMock.Object, ServiceLifetime.Singleton);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+
+            instanceProviderMock.Verify();
+        }
+
+        [TestMethod]
+        public void AddInstanceProxy_GenericInstance_Crate()
+        {
+            Mock<IInstanceProvicer> instanceProviderMock = new Mock<IInstanceProvicer>(MockBehavior.Strict);
+            instanceProviderMock.Setup(t => t.GetInstance()).Returns(new MessageService()).Verifiable();
+
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy<IMessageService>(instanceProviderMock.Object);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+
+            instanceProviderMock.Verify();
+        }
+
+        [TestMethod]
+        public void AddInstanceProxy_GenericInstanceWithlifestyle_Crate()
+        {
+            Mock<IInstanceProvicer> instanceProviderMock = new Mock<IInstanceProvicer>(MockBehavior.Strict);
+            instanceProviderMock.Setup(t => t.GetInstance()).Returns(new MessageService()).Verifiable();
+
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy<IMessageService>(instanceProviderMock.Object, ServiceLifetime.Singleton);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+
+            instanceProviderMock.Verify();
+        }
+
+        [TestMethod]
+        public void AddInstanceProxy_TypeFactory_Crate()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy(typeof(IMessageService), () => new MessageService());
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+        }
+
+        [TestMethod]
+        public void AddInstanceProxy_TypeFactoryWithLifeStyle_Crate()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy(typeof(IMessageService), () => new MessageService(), ServiceLifetime.Singleton);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+        }
+
+        [TestMethod]
+        public void AddInstanceProxy_GenericFactory_Crate()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy(typeof(IMessageService), () => new MessageService());
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+        }
+
+        [TestMethod]
+        public void AddInstanceProxy_GenericFactoryWithLifeStyle_Crate()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstanceProxy(typeof(IMessageService), () => new MessageService(), ServiceLifetime.Singleton);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IMessageService typeA = serviceProvider.GetRequiredService<IMessageService>();
+            typeA.ShouldNotBeNull();
+            typeA.ShouldNotBeOfType<MessageService>();
+
+            typeA.Send("test@test.sk", "body");
+        }
+    }
+}
--- src/MassiveDynamicProxyGenerator.Microsoft.DependencyInjection/ServiceCollectionExtensions.InstanceProxy.cs
+++ src/MassiveDynamicProxyGenerator.Microsoft.DependencyInjection/ServiceCollectionExtensions.InstanceProxy.cs
@@ -7,5 +7,48 @@ namespace MassiveDynamicProxyGenerator.Microsoft.DependencyInjection
 {
     public static partial class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddInstanceProxy(this IServiceCollection serviceCollection, Type serviceType, IInstanceProvicer instanceProvicer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IServiceCollection AddInstanceProxy<TService>(this IServiceCollection serviceCollection, IInstanceProvicer instanceProvicer)
+            where TService : class
+        {
+            return serviceCollection.AddInstanceProxy(typeof(TService), instanceProvicer);
+        }
+
+        public static IServiceCollection AddInstanceProxy(this IServiceCollection serviceCollection, Type serviceType, IInstanceProvicer instanceProvicer, ServiceLifetime proxyLifetime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IServiceCollection AddInstanceProxy<TService>(this IServiceCollection serviceCollection, IInstanceProvicer instanceProvicer, ServiceLifetime proxyLifetime)
+              where TService : class
+        {
+            return serviceCollection.AddInstanceProxy(typeof(TService), instanceProvicer, proxyLifetime);
+        }
+
+        public static IServiceCollection AddInstanceProxy(this IServiceCollection serviceCollection, Type serviceType, Func<object> instanceProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IServiceCollection AddInstanceProxy<TService>(this IServiceCollection serviceCollection, Func<TService> instanceProvider)
+             where TService : class
+        {
+            return serviceCollection.AddInstanceProxy(typeof(TService), instanceProvider);
+        }
+
+        public static IServiceCollection AddInstanceProxy(this IServiceCollection serviceCollection, Type serviceType, Func<object> instanceProvider, ServiceLifetime proxyLifetime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IServiceCollection AddInstanceProxy<TService>(this IServiceCollection serviceCollection, Func<TService> instanceProvider, ServiceLifetime proxyLifetime)
+            where TService : class
+        {
+            return serviceCollection.AddInstanceProxy(typeof(TService), instanceProvider, proxyLifetime);
+        }
     }
 }
