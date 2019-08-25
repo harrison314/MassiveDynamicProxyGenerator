f9e5e5bc7f6ae8549b911b53b47c949f7d1edfdc
harrison314 <harrison314@azet.sk>
2019-08-25 19:37:21 +0200

    Add MockInterceptor. #7

---
+++ src/Src/MassiveDynamicProxyGenerator/MockInterceptor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveDynamicProxyGenerator
+{
+    /// <summary>
+    /// Interceptor for mocking types instances.
+    /// </summary>
+    /// <seealso cref="IInterceptor"/>
+    public class MockInterceptor : IInterceptor
+    {
+        private readonly Func<Type, object> returnValueFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MockInterceptor"/> class.
+        /// </summary>
+        /// <param name="returnValues">
+        /// The dictionary for mapping return type to value.
+        /// If dictionary does not contain return type, then interceptor returns default value of type.
+        /// </param>
+        /// <exception cref="ArgumentNullException">returnValues</exception>
+        public MockInterceptor(Dictionary<Type, object> returnValues)
+        {
+            if (returnValues == null)
+            {
+                throw new ArgumentNullException(nameof(returnValues));
+            }
+
+            this.returnValueFactory = type => returnValues.TryGetValue(type, out object value) ? value : null;
+        }
+
+#if !NET40
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MockInterceptor"/> class.
+        /// </summary>
+        /// /// <param name="returnValues">
+        /// The dictionary for mapping return type to value.
+        /// If dictionary does not contain return type, then interceptor returns default value of type.
+        /// </param>
+        /// <exception cref="ArgumentNullException">returnValues</exception>
+        public MockInterceptor(IReadOnlyDictionary<Type, object> returnValues)
+        {
+            if (returnValues is null)
+            {
+                throw new ArgumentNullException(nameof(returnValues));
+            }
+
+            this.returnValueFactory = type => returnValues.TryGetValue(type, out object value) ? value : null;
+        }
+#endif
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MockInterceptor"/> class.
+        /// </summary>
+        /// <param name="returnValueFactory">
+        /// The function return value by type.
+        /// If function return <c>null</c> interceptor return default value of type.
+        /// </param>
+        /// <exception cref="ArgumentNullException">returnValueFactory</exception>
+        public MockInterceptor(Func<Type, object> returnValueFactory)
+        {
+            this.returnValueFactory = returnValueFactory ?? throw new ArgumentNullException(nameof(returnValueFactory));
+        }
+
+        /// <summary>
+        /// Intercept call of method.
+        /// </summary>
+        /// <param name="invocation">Invocation informations.</param>
+        public void Intercept(IInvocation invocation)
+        {
+            object returnValue = this.returnValueFactory.Invoke(invocation.ReturnType);
+            if (returnValue != null)
+            {
+                invocation.ReturnValue = returnValue;
+            }
+        }
+    }
+}
---
+++ src/Test/MassiveDynamicProxyGenerator.Tests/MockInterceptorTests.cs
@@ -0,0 +1,125 @@
+﻿using MassiveDynamicProxyGenerator.Tests.TestInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveDynamicProxyGenerator.Tests
+{
+    [TestClass]
+    public class MockInterceptorTests
+    {
+        [TestMethod]
+        public void MockInterceptor_ReturnInDictionary_ReturnDefault()
+        {
+            ProxyGenerator generator = new ProxyGenerator();
+            IReturnTypes instance = generator.GenerateProxy<IReturnTypes>(new MockInterceptor(new Dictionary<Type, object>()));
+
+            instance.ShouldNotBeNull();
+
+            instance.CreateSb(string.Empty).ShouldBe(default(StringBuilder));
+            instance.GetLength(string.Empty).ShouldBe(default(int));
+            instance.GetVoid();
+        }
+
+        [TestMethod]
+        public void MockInterceptor_ReturnInFunction_ReturnDefault()
+        {
+            ProxyGenerator generator = new ProxyGenerator();
+            IReturnTypes instance = generator.GenerateProxy<IReturnTypes>(new MockInterceptor(type => null));
+
+            instance.ShouldNotBeNull();
+
+            instance.CreateSb(string.Empty).ShouldBe(default(StringBuilder));
+            instance.GetLength(string.Empty).ShouldBe(default(int));
+            instance.GetVoid();
+        }
+
+        [TestMethod]
+        public void MockInterceptor_ReturnInDictionary_ReturnInt()
+        {
+            Dictionary<Type, object> mockValues = new Dictionary<Type, object>()
+            {
+                { typeof(int), 42 }
+            };
+
+            ProxyGenerator generator = new ProxyGenerator();
+            IReturnTypes instance = generator.GenerateProxy<IReturnTypes>(new MockInterceptor(mockValues));
+
+            instance.ShouldNotBeNull();
+
+            instance.CreateSb(string.Empty).ShouldBe(default(StringBuilder));
+            instance.GetLength(string.Empty).ShouldBe(42);
+            instance.GetVoid();
+        }
+
+        [TestMethod]
+        public void MockInterceptor_ReturnInFunction_ReturnInt()
+        {
+            Func<Type, object> retValueFactory = type =>
+            {
+                if (type == typeof(int))
+                {
+                    return 42;
+                }
+
+                return null;
+            };
+
+            ProxyGenerator generator = new ProxyGenerator();
+            IReturnTypes instance = generator.GenerateProxy<IReturnTypes>(new MockInterceptor(retValueFactory));
+
+            instance.ShouldNotBeNull();
+
+            instance.CreateSb(string.Empty).ShouldBe(default(StringBuilder));
+            instance.GetLength(string.Empty).ShouldBe(42);
+            instance.GetVoid();
+        }
+
+        [TestMethod]
+        public void MockInterceptor_ReturnInDictionary_ReturnStringBuilder()
+        {
+            StringBuilder defaultSb = new StringBuilder("hello");
+            Dictionary<Type, object> mockValues = new Dictionary<Type, object>()
+            {
+                { typeof(StringBuilder), defaultSb }
+            };
+
+            ProxyGenerator generator = new ProxyGenerator();
+            IReturnTypes instance = generator.GenerateProxy<IReturnTypes>(new MockInterceptor(mockValues));
+
+            instance.ShouldNotBeNull();
+
+            instance.CreateSb(string.Empty).ShouldBe(defaultSb);
+            instance.GetLength(string.Empty).ShouldBe(default(int));
+            instance.GetVoid();
+        }
+
+        [TestMethod]
+        public void MockInterceptor_ReturnInFunction_ReturnStringBuilder()
+        {
+            StringBuilder defaultSb = new StringBuilder("hello");
+            Func<Type, object> retValueFactory = type =>
+            {
+                if (type == typeof(StringBuilder))
+                {
+                    return defaultSb;
+                }
+
+                return null;
+            };
+
+            ProxyGenerator generator = new ProxyGenerator();
+            IReturnTypes instance = generator.GenerateProxy<IReturnTypes>(new MockInterceptor(retValueFactory));
+
+            instance.ShouldNotBeNull();
+
+            instance.CreateSb(string.Empty).ShouldBe(defaultSb);
+            instance.GetLength(string.Empty).ShouldBe(default(int));
+            instance.GetVoid();
+        }
+    }
+}
