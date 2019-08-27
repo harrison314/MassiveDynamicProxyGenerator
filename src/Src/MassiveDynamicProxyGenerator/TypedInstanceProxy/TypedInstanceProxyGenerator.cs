a1196d6c05f5351cacbfb543b8a7c498094acef2
harrison314 <harrison314@azet.sk>
2019-08-27 21:25:16 +0200

    Fix style.

--- src/Src/MassiveDynamicProxyGenerator/TypedInstanceProxy/TypedInstanceProxyGenerator.cs
+++ src/Src/MassiveDynamicProxyGenerator/TypedInstanceProxy/TypedInstanceProxyGenerator.cs
@@ -1,11 +1,11 @@
-﻿using MassiveDynamicProxyGenerator.Utils;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using MassiveDynamicProxyGenerator.Utils;
 
 namespace MassiveDynamicProxyGenerator.TypedInstanceProxy
 {
